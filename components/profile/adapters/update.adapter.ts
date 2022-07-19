7c9fbf17dce7358dd18ece81b7548630ec2b9daf
Fuego <fgomez@dexfreight.io>
2022-07-19 10:09:25 -0500

    update company info

--- components/profile/adapters/update.adapter.ts
+++ components/profile/adapters/update.adapter.ts
@@ -1,18 +1,31 @@
-import { httpPost, httpPut } from '../../../utils/http-client';
+import { httpGet, httpPost, httpPut } from '../../../utils/http-client';
 import getConfig from 'next/config';
 import { getToken } from 'utils/helpers';
 
-export const updateAdapter: any = async (data: any) => {
-    try {
-        const { TEAMWORK } = getConfig().publicRuntimeConfig;
-        const token = getToken()
+const { TEAMWORK, LOGIN_URL } = getConfig().publicRuntimeConfig;
+const token = getToken()
 
-        const response = await httpPut(`http://ec2-18-233-75-58.compute-1.amazonaws.com:9040/company/api/organization/v1/update`, data, token);
+export const updateAdapter: any = async (id:string, data: any) => {
+    try {
+        let path = `company/api/user/v1/${id}/update`
+
+        const response = await httpPut(`${LOGIN_URL}/${path}`, data, token);
         console.log(response)
         return response;
     } catch (e) {
-        console.error( 'err', e);
+        console.error('err', e);
     }
 }
 
+export const getCompanyInfo = async (id: string) => {
+    try {
+        let path = `company/api/user/v1/${id}/organization`;
+        
+        const response = await httpGet(`${LOGIN_URL}/${path}`, token);
 
+        console.log(response)
+        return response;
+    } catch (e) {
+        console.error(e)
+    }
+}
