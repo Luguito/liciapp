4c38dca69cf5267ded95c6d040d8c7770bee57fd
Fuego <fgomez@dexfreight.io>
2022-07-19 10:26:59 -0500

    update company info

--- components/profile/company.tsx
+++ components/profile/company.tsx
@@ -1,44 +1,29 @@
 import { useState, useEffect } from 'react';
-import { TitleCreate, ContainerFlex, TextArea, ListDocuments, TitleDocuments, PurpleButton, ElementList, DocumentName } from '@global-styled';
-import { TextField } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
+import { TitleCreate, ContainerFlex, TextArea, PurpleButton } from '@global-styled';
+import { Alert, Snackbar, TextField } from '@mui/material';
 import { schema } from './creat.schema';
-import { navigateTo, getKey} from '@utils/helpers';
-import { updateAdapter } from './adapters/update.adapter';
-
-// import { } from './company.styled';
-
+import { getKey } from '@utils/helpers';
+import { updateAdapter, getCompanyInfo } from './adapters/update.adapter';
 
 export function CompanyProfile() {
-    const [user, setUser] = useState<any>({});
-    const [profile, seProfile] = useState<any>({});
+    const [profile, setProfile] = useState<any>({});
+    const [openToast, setOpenToast] = useState<any>(false);
+    const orgID = JSON.parse(getKey('user'))["organization-id"]
+    const license =  JSON.parse(getKey('user'))["permission-name"]
 
 
-    useEffect(()=> {
-        const userData = JSON.parse(getKey('user'));
-        setUser(userData);
+    useEffect(() => {
+        getCompanyInfo(orgID).then(({ body }) => setProfile({
+            ...body,
+            license: license
+        }))
+    }, []);
 
-    }, [])
-    // const [project, setProject] = useState({
-    //     "organization-id": "",
-    //     "email": "",
-    //     "license": "",
-    //     "name": "",
-    //     "details": "",
-    //     "legal-id": "",
-    //     "address": "",
-    //     "url": "",
-    //     "cellphone": "",
-    //     "phone": ""
-    // });
-
-    const setValues = (label: string, value: string) => seProfile({ ...profile, [label]: value });
+    const setValues = (label: string, value: string) => setProfile({ ...profile, [label]: value });
 
     const handleSubmit = () => {
-        profile['organization-id'] = user['organization-id'];
-        schema.isValid(profile).then(async v => await updateAdapter(profile));
-        console.log(schema.isValid(profile))
-        // navigateTo('/hojas-de-vida')
+        schema.isValid(profile).then(async v => await updateAdapter(orgID, profile));
+        setOpenToast(true);
     };
 
     return (
@@ -52,26 +37,26 @@ export function CompanyProfile() {
                 <TextField value={profile['email']} fullWidth placeholder="Email" size="small" onChange={(e) => setValues('email', e.target.value)}></TextField>
             </ContainerFlex>
             <ContainerFlex>
-                <TextField value={profile['legal-rep']}  fullWidth placeholder="Representante Legal" size="small" onChange={(e) => setValues('legal-rep', e.target.value)}></TextField>
+                <TextField value={profile['legal-rep']} fullWidth placeholder="Representante Legal" size="small" onChange={(e) => setValues('legal-rep', e.target.value)}></TextField>
                 <TextField value={profile['city']} fullWidth placeholder="Ciudad" size="small" onChange={(e) => setValues('city', e.target.value)}></TextField>
             </ContainerFlex>
             <ContainerFlex>
                 <TextField value={profile['country']} fullWidth placeholder="Pais" size="small" onChange={(e) => setValues('country', e.target.value)}></TextField>
-                <TextField value={profile['address']}  fullWidth placeholder="Dirección" size="small" onChange={(e) => setValues('address', e.target.value)}></TextField>
+                <TextField value={profile['address']} fullWidth placeholder="Dirección" size="small" onChange={(e) => setValues('address', e.target.value)}></TextField>
             </ContainerFlex>
             <ContainerFlex>
-                <TextField value={profile['category']}  fullWidth placeholder="Categoria" size="small" onChange={(e) => setValues('category', e.target.value)}></TextField>
+                <TextField value={profile['category']} fullWidth placeholder="Categoria" size="small" onChange={(e) => setValues('category', e.target.value)}></TextField>
                 <TextField value={profile['specialty']} fullWidth placeholder="Especialidad" size="small" onChange={(e) => setValues('specialty', e.target.value)}></TextField>
             </ContainerFlex>
             <ContainerFlex>
-                <TextField value={profile['cellphone']}  fullWidth placeholder="Celular" size="small" onChange={(e) => setValues('cellphone', e.target.value)}></TextField>
+                <TextField value={profile['cellphone']} fullWidth placeholder="Celular" size="small" onChange={(e) => setValues('cellphone', e.target.value)}></TextField>
                 <TextField value={profile['phone']} fullWidth placeholder="Telefono" size="small" onChange={(e) => setValues('phone', e.target.value)}></TextField>
             </ContainerFlex>
             <ContainerFlex>
-                <TextField value={profile['url']}  fullWidth placeholder="Url" size="small" onChange={(e) => setValues('url', e.target.value)}></TextField>
+                <TextField value={profile['url']} fullWidth placeholder="Url" size="small" onChange={(e) => setValues('url', e.target.value)}></TextField>
                 <TextField value={profile['license']} fullWidth placeholder="Tipo" size="small" onChange={(e) => setValues('license', e.target.value)}></TextField>
             </ContainerFlex>
-            <TextArea value={profile['details']}  placeholder="Descripción" onChange={(e) => setValues('details', e.target.value)}></TextArea> 
+            <TextArea value={profile['details']} placeholder="Descripción" onChange={(e) => setValues('details', e.target.value)}></TextArea>
             {/* <TitleDocuments>
                 Documentacion
                 <PurpleButton>Agregar Documento</PurpleButton>
@@ -90,7 +75,12 @@ export function CompanyProfile() {
                     <DocumentName>Documento</DocumentName>
                 </ElementList>
             </ListDocuments> */}
-            <PurpleButton style={{ marginTop: '2em' }} onClick={ () => handleSubmit()}>Guardar</PurpleButton>
+            <PurpleButton style={{ marginTop: '2em' }} onClick={() => handleSubmit()}>Guardar</PurpleButton>
+            <Snackbar open={openToast} autoHideDuration={6000} onClose={() => setOpenToast(false)}>
+                <Alert onClose={() => setOpenToast(false)} severity="success" sx={{ width: '100%' }}>
+                    Compañia actualizada
+                </Alert>
+            </Snackbar>
         </>
     )
 }
